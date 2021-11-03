db5b498415b9dbc81170797e90bd7e60352cb98a
mihai-dulgheru <63505058+mihai-dulgheru@users.noreply.github.com>
2021-11-03 11:01:05 +0200

    last commit

--- seminar-05/2-a-crypto/index.js
+++ seminar-05/2-a-crypto/index.js
@@ -11,3 +11,13 @@ let bytes = crypto.AES.decrypt(crypt, "secret key 123");
 let decrypt = JSON.parse(bytes.toString(crypto.enc.Utf8));
 
 console.log(decrypt);
+
+const KEY_WORD_2 = 'mysecretkey2';
+let crypt2 = crypto.AES.encrypt(JSON.stringify(word2), KEY_WORD_2).toString();
+
+console.log(crypt2);
+
+let bytes2 = crypto.AES.decrypt(crypt2, KEY_WORD_2);
+let decrypt2 = JSON.parse(bytes2.toString(crypto.enc.Utf8));
+
+console.log(decrypt2);
